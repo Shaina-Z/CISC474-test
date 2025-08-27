f556471cfa8a414aaefb699f32a02caf105f0790
Shaina <shainaz@udel.edu>
2025-08-27 19:52:45 -0400

    there is now a cat that can be added

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const buttonContainer=document.getElementById('buttonContainer');
+const catContainer=document.getElementById('catContainer');
 let button = document.createElement("button");
 button.textContent="CLICK THE BUTTON";
 buttonContainer.appendChild(button);
+button.addEventListener('click', () => {
+        let cat=document.createElement('img')
+        cat.src = 'cat.jpeg'
+        catContainer.appendChild(cat);
+    })
