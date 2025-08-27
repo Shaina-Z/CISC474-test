cfceb39091bdba218c69cc019057f0bfd03fc446
Shaina <shainaz@udel.edu>
2025-08-27 16:37:58 -0400

    added button

---
+++ index.js
@@ -0,0 +1,4 @@
+const buttonContainer=document.getElementById('buttonContainer');
+let button = document.createElement("button");
+button.textContent="CLICK THE BUTTON";
+buttonContainer.appendChild(button);
